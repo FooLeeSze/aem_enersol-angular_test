fec2522fe84f1968ac99ad8e32d86a2907c55335
Jaclyn-Foo <leeszefoo@gmail.com>
2022-07-01 16:54:07 +0800

    Add index and App files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import DashBoard from './Components/Dashboard';
+import SignIn from './Components/SignIn';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
